e4a1c999245c33664ba888c8d90b9628766f5d59
mcmah309 <dillonmcmahon79@gmail.com>
2024-07-17 19:31:56 +0000

    feat: initial cli scaffold

---
+++ src/cli.rs
@@ -0,0 +1,62 @@
+use clap::{Parser, Subcommand};
+
+/// yard
+/// 
+/// yard is the cli tool for Container Yard - a declarative reusable decentralized approach for defining containers.
+#[derive(Parser)]
+#[clap(author = "Henry McMahon", version = "0.1.0", about = "A declarative reusable decentralized approach for defining containers", long_about = None)]
+pub struct Cli {
+    #[clap(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Build Containerfiles from a `yard.yaml` file
+    Build {
+        /// Path to the `yard.yaml` file
+        #[clap(default_value = ".")]
+        path: String,
+    },
+    /// Initialize a `yard.yaml` file
+    Init {
+        /// Path to initialize the `yard.yaml` file
+        #[clap(default_value = ".")]
+        path: String,
+        /// Template to use for initialization
+        #[clap(short, long)]
+        template: Option<String>,
+    },
+    /// Save the current `yard.yaml` file as a template
+    Save {
+        /// Path to the `yard.yaml` file
+        #[clap(default_value = ".")]
+        path: String,
+        /// Name of the template to save as
+        #[clap(short, long)]
+        template: Option<String>,
+        /// Remote repository. Format `<REF> <REPO_URL>`.
+        #[clap(short, long, value_parser = validate_remote)]
+        remote: Vec<String>,
+    },
+    /// List
+    List {
+        /// List saved templates
+        #[clap(short, long,)] // conflicts_with = "templates", requires = "templates"
+        templates: bool,
+    },
+    /// Delete a template
+    Delete {
+        /// Name of the template to delete
+        #[clap(short, long)]
+        template: String,
+    },
+}
+
+fn validate_remote(val: &str) -> Result<String, String> {
+    let parts: Vec<&str> = val.split_whitespace().collect();
+    if parts.len() != 2 {
+        return Err(String::from("Remote must be exactly 2 elements: <REF> <REPO_URL>"));
+    }
+    Ok(String::from(val))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
+use std::path::Path;
+
+use clap::Parser;
+use cli::{Cli, Commands};
+use gix::{Remote, Repository};
+
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Build { path } => {
+            println!("Building Containerfiles from {}", path);
+        },
+        Commands::Init { path, template } => {
+            match template {
+                Some(t) => println!("Initializing {} with template {}", path, t),
+                None => println!("Initializing {}", path),
+            }
+        },
+        Commands::Save { path, template , remote} => {
+            let template_name = template.unwrap_or_else(|| {
+                let path = std::env::current_dir().expect("Failed to get current directory");
+                path.parent().map(|e| e.file_name()).flatten().expect("Failed to get current directory name").to_str().expect("Failed to convert to string").to_string()
+            });
+            if remote.is_empty() {
+                save_local_yard_file_as_template(path, template_name);
+            }
+            else {
+                assert!(remote.len() == 2);
+                let ref_ = remote[0].to_string();
+                let url = remote[1].to_string();
+                save_remote_yard_file_as_template(path, template_name, ref_, url);
+            }
+        },
+        Commands::List { templates } => {
+            // Handle listing available templates
+            if templates {
+                println!("Listing templates");
+            }
+        },
+        Commands::Delete { template } => {
+            // Handle deleting a template
+            println!("Deleting template {}", template);
+        },
+    }
 }
+
+fn save_local_yard_file_as_template<P: AsRef<Path>>(path: P, template_name: String) {
+    let path = path.as_ref();
+}
+
+fn save_remote_yard_file_as_template<P: AsRef<Path>>(path: P, template_name: String, reference: String, url: String) {
+    let path = path.as_ref();
+}
+
+// fn main2() -> Result<(), Box<dyn std::error::Error>> {
+//     let url = "https://github.com/your/repo.git";
+//     let repo_path = "/path/to/local/repo";
+    
+//     let repo = gix::prepare_clone(url, path);
+
+//     println!("Repository cloned to: {:?}", repo.work_dir());
+
+//     Ok(())
+// }
