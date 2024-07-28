bc0518b52965f968746868ac27e0f30196417f2e
mcmah309 <dillonmcmahon79@gmail.com>
2024-07-28 18:15:03 +0000

    chore: Update scripts

--- scripts/debian/create_and_install_deb.sh
+++ scripts/debian/create_and_install_deb.sh
@@ -1,9 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
 
 # Resulting from https://medium.com/rust-programming-language/simplifying-debian-packaging-for-rust-a-step-by-step-guide-for-rust-developers-0457cdb3c81d
-# Package config in Cargo.toml - package.metadata.deb
+# Package config is located in Cargo.toml's [package.metadata.deb] section
+
+# version=0.0.0
 cargo install cargo-deb
 cargo deb
 ## Install
-# dpkg -i ./target/debian/containeryard_<VERSION_HERE>.deb
+# dpkg -i ./target/debian/containeryard_$version.deb
 ## Uninstall
 # sudo dpkg -r containeryard
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 #version=v0.0.0
 #git tag --delete $version
 git tag -a $version -m "$version"
